1590e0bf1fc2c3cfcbeaff8c374066bbbe5173a3
Doug Arley <sgtrama@gmail.com>
2020-11-29 18:46:42 -0500

    Update most LUAs

---
+++ WTF/Account/YOUR ACCOUNT NAME/SavedVariables/Prat-3.0.lua
@@ -0,0 +1,180 @@
+
+Prat3DB = {
+	["namespaces"] = {
+		["Prat_ChannelColorMemory"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["colors"] = {
+						["community:20321:1"] = {
+							["r"] = 1,
+							["g"] = 0.5137255191802979,
+							["b"] = 0.3803921937942505,
+						},
+						["community:1451951:1"] = {
+							["r"] = 0.5098039507865906,
+							["g"] = 0.7725490927696228,
+							["b"] = 1,
+						},
+					},
+				},
+			},
+		},
+		["Prat_Fading"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["duration"] = 14,
+				},
+			},
+		},
+		["Prat_ChatLink"] = {
+			["profiles"] = {
+				["Default"] = {
+				},
+			},
+		},
+		["Prat_Sounds"] = {
+		},
+		["Prat_Mentions"] = {
+		},
+		["Prat_CopyChat"] = {
+		},
+		["Prat_PopupMessage"] = {
+		},
+		["Prat_ServerNames"] = {
+		},
+		["Prat_UrlCopy"] = {
+		},
+		["Prat_AltNames"] = { },
+		["Prat_PlayerNames"] = {
+		},
+		["Prat_Memory"] = {
+		},
+		["Prat_Frames"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["minchatwidthdefault"] = 296,
+					["maxchatheightdefault"] = 400,
+					["maxchatwidthdefault"] = 608.0000610351562,
+					["initialized"] = true,
+				},
+			},
+		},
+		["Prat_Editbox"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["edgeSize"] = 1,
+					["editX"] = 710.002075195313,
+					["editW"] = 497.999877929688,
+					["attach"] = "LOCK",
+					["border"] = "None",
+					["tileSize"] = 11,
+					["borderColor"] = {
+						["a"] = 0,
+						["r"] = 0,
+						["g"] = 0,
+						["b"] = 0,
+					},
+					["editY"] = 234.815505981445,
+					["background"] = "None",
+					["backgroundColor"] = {
+						["a"] = 0,
+					},
+					["inset"] = 1,
+				},
+			},
+		},
+		["Prat_History"] = {
+		},
+		["Prat_SMFHax"] = {
+			["profiles"] = {
+				["Default"] = {
+				},
+			},
+		},
+		["Prat_Font"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["size"] = {
+						["ChatFrame4"] = 10,
+						["ChatFrame1"] = 10,
+					},
+				},
+			},
+		},
+		["Prat_Bubbles"] = {
+		},
+		["Prat_ChannelNames"] = {
+		},
+		["Prat_TellTarget"] = {
+		},
+		["Prat_Paragraph"] = {
+		},
+		["Prat_ChannelSticky"] = {
+		},
+		["Prat_Alias"] = {
+		},
+		["Prat_OriginalButtons"] = {
+		},
+		["Prat_Highlight"] = {
+		},
+		["Prat_KeyBindings"] = {
+		},
+		["Prat_Scroll"] = {
+		},
+		["Prat_Invites"] = {
+		},
+		["Prat_Achievements"] = {
+		},
+		},
+		["Prat_Timestamps"] = {
+		},
+		["Prat_ChatLog"] = {
+		},
+		["Prat_Search"] = {
+		},
+		["Prat_Buttons"] = {
+			["profiles"] = {
+				["Console UI"] = {
+					["showvoice"] = false,
+					["scrollReminder"] = false,
+					["showchannel"] = false,
+					["showBnet"] = false,
+					["showButtons"] = false,
+					["showminimize"] = false,
+					["showMenu"] = false,
+				},
+			},
+		},
+		["Prat_ChatTabs"] = {
+			["profiles"] = {
+				["Default"] = {
+					["on"] = true,
+				},
+				["Console UI"] = {
+					["on"] = true,
+				},
+			},
+		},
+		["Prat_HoverTips"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Ramaniscence - Bleeding Hollow"] = "Default",
+	},
+	["profiles"] = {
+		["Console UI"] = {
+			["modules"] = {
+				["ChatLog"] = 2,
+				["Mentions"] = 2,
+				["ChatTabs"] = 3,
+				["PopupMessage"] = 2,
+				["AltNames"] = 2,
+				["Sounds"] = 2,
+				["Paragraph"] = 2,
+				["KeyBindings"] = 2,
+				["OriginalButtons"] = 2,
+				["Alias"] = 2,
+			},
+		},
+	},
+}
